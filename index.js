a381852558824a79190756bc69f79cefe1b9dfe5
Puneet Anujuri <panjuri@umail.iu.edu>
2017-05-15 16:02:08 -0400

    ColorDiv

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ const personForm = document.querySelector('form')
 personForm.onsubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
-    console.log(form.personName)
+    const details = document.querySelector('.details')
+
+    const personName = form.personName.value
+    const hairColor = form.hairColor.value
+
+    const em = document.createElement('em')
+    em.textContent = personName
+
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = hairColor
+    colorDiv.style.height = '50px'
+    colorDiv.style.width = '100px'
+
+    details.appendChild(em)
+    details.appendChild(colorDiv)
 }
