4ac457ab8f1eb1a9adc0376c57123e0281cc4a7a
Puneet Anujuri <panjuri@umail.iu.edu>
2017-05-16 10:44:26 -0400

    Complete Day 1

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const personForm = document.querySelector('form')
 
-personForm.onsubmit = (ev) => {
+const renderColor = (hairColor) => {
+    const colorDiv = document.createElement('li')
+    colorDiv.style.backgroundColor = hairColor
+    colorDiv.style.height = '50px'
+    colorDiv.style.width = '100px'
+    return colorDiv
+}
+const handleSubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
     const details = document.querySelector('.details')
 
     const personName = form.personName.value
     const hairColor = form.hairColor.value
+    const age = form.age.value
+    const birthplace = form.birthplace.value
 
-    const em = document.createElement('em')
-    em.textContent = personName
+    const name = document.createElement('li')
+    name.textContent = personName
 
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = hairColor
-    colorDiv.style.height = '50px'
-    colorDiv.style.width = '100px'
+    const colorDiv = renderColor(hairColor)
 
-    details.appendChild(em)
+    const years = document.createElement('li')
+    years.textContent = age 
+
+    const placebirth = document.createElement('li')
+    placebirth.textContent = birthplace 
+
+    
+
+    details.appendChild(name)
     details.appendChild(colorDiv)
-}
+    details.appendChild(years)
+    details.appendChild(placebirth)
+    }
+personForm.addEventListener('submit',handleSubmit)
